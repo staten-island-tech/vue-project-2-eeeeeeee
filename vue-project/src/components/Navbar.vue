4f1aa2220f754f4380c00cb3f6825d8f4e5bdbbc
Eric2742 <eric.nguyenlin22@sitechhs.com>
2022-05-11 09:51:01 -0400

    .

--- vue-project/src/components/Navbar.vue
+++ vue-project/src/components/Navbar.vue
@@ -1,14 +1,19 @@
 <template>
   <div>
     <div class="nav">
-      <div class="nav-link">
-        <router-link to="/">Home</router-link>
-      </div>
-      <div class="nav-link">
-        <router-link to="/login">Login</router-link>
-      </div>
-      <div class="nav-link">
-        <router-link to="/signup">Sign Up</router-link>
+      <button class="title">
+        <router-link to="/">Yu-Gi-Oh Deck Maker</router-link>
+      </button>
+      <div class="options">
+        <div class="nav-link">
+          <router-link to="/">Home</router-link>
+        </div>
+        <div class="nav-link">
+          <router-link to="/login">Login</router-link>
+        </div>
+        <div class="nav-link">
+          <router-link to="/signup">Sign Up</router-link>
+        </div>
       </div>
     </div>
   </div>
@@ -26,9 +31,24 @@ export default {};
   padding-left: 5%;
   font-size: 2rem;
   letter-spacing: 0.1rem;
+  height: 15rem;
+  align-items: center;
+  background-color: bisque;
+}
+
+.title {
+  font-size: 6rem;
+  float: left;
+  max-height: 100%;
+}
+
+.options {
   display: flex;
   justify-content: space-between;
+  width: 65%;
+  float: right;
 }
+
 .nav-link {
   font-size: 2rem;
 }
